898c8627d971e3d1a1931ca3de1fed168f1d15f9
Olivier Lasne <lasneolivier@gmail.com>
2025-05-20 22:29:26 +0200

    refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,26 +15,22 @@ async fn main() {
     if let Ok(listener) = tokio::net::TcpListener::bind("127.0.0.1:3000").await {
         println!("Listening on port {}", "3000".cyan());
         
-        let (listener_tx, listener_rx) = mpsc::channel::<Vec<u8>>(32);
-        // let (remote_tx, remote_rx) = mpsc::channel::<Vec<u8>>(32);
-
-
-        tokio::spawn(async move {
-            forward_http_requests(listener_rx).await
-        });
-
         loop {
             if let Ok((socket, addr)) = listener.accept().await {
-                let (socket_read, socket_write) = split(socket);
-
                 println!("Accepted connection from {}", addr.to_string().green());
+                let (socket_read, socket_write) = split(socket);
+                
+                let (listener_tx, listener_rx) = mpsc::channel::<Vec<u8>>(32);
+                // let (remote_tx, remote_rx) = mpsc::channel::<Vec<u8>>(32);
 
-                let listener_tx = listener_tx.clone();
                 tokio::spawn(async move {
                     receive_http_requests(socket_read, listener_tx).await
                 });
 
-                
+                tokio::spawn(async move {
+                    forward_http_requests(listener_rx).await
+                });
+
             }
         }
     } else {
